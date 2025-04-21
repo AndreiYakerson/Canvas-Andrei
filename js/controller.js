8eebb5a04b6d3229324cf5d797f6d7367679d801
AndreiYakerson <tubikwheels@gmail.com>
2025-04-21 22:15:04 +0300

    Add resize canvas

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,11 @@
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
 }
 
+function resizeCanvas() { 
+    const elContainer = document.querySelector('.canvas-container') 
+    gElCanvas.width = elContainer.clientWidth 
+} 
