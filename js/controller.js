9e059cb7e02e2f4d7a6edf588611b76818af1b85
AndreiYakerson <tubikwheels@gmail.com>
2025-04-22 22:34:13 +0300

    Add upload img

--- js/controller.js
+++ js/controller.js
@@ -70,3 +70,28 @@ function onDownloadCanvas(el) {
     el.href = dataUrl
 }
 
+function onImgInput(ev) {
+    loadImageFromInput(ev,renderImg)
+}
+
+function loadImageFromInput(ev,onImageReady) {
+    document.querySelector('canvas').innerHTML = ''
+    const reader = new FileReader()
+
+    reader.onload = function (event) {
+        const img = new Image()
+        img.onload = () => {
+            onImageReady(img)
+        }
+        img.src = event.target.result
+    }
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+function renderImg(img) {
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(img,0,0,gElCanvas.width,gElCanvas.height)
+}
+
+
+
